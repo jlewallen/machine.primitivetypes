4b260605ce72ba43066885c8f5ceb6f5c56cd167
Jacob Lewallen <jlewalle@gmail.com>
2010-01-23 14:42:17 -0800

    Non-generic entity translator factory.

--- Machine.PrimitiveTypes.MongoDb/ITranslatorLocator.cs
+++ Machine.PrimitiveTypes.MongoDb/ITranslatorLocator.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace Machine.PrimitiveTypes.MongoDb
 {
   public interface ITranslatorLocator
   {
     ITranslateEntities<T> FindEntityTranslator<T>();
+    ITranslateEntities<object> FindEntityTranslator(Type type);
   }
 }
--- Machine.PrimitiveTypes.MongoDb/TranslatorLocator.cs
+++ Machine.PrimitiveTypes.MongoDb/TranslatorLocator.cs
@@ -1,4 +1,5 @@
 using System;
+using MongoDB.Driver;
 
 namespace Machine.PrimitiveTypes.MongoDb
 {
@@ -7,6 +8,34 @@ namespace Machine.PrimitiveTypes.MongoDb
     public ITranslateEntities<T> FindEntityTranslator<T>()
     {
       return (ITranslateEntities<T>)Activator.CreateInstance(typeof(DefaultEntityTranslator<>).MakeGenericType(typeof(T)));
+    }
+
+    public ITranslateEntities<object> FindEntityTranslator(Type type)
+    {
+      var translator = Activator.CreateInstance(typeof(DefaultEntityTranslator<>).MakeGenericType(type));
+      var wrapperType = typeof(TranslateEntitiesWrapper<>).MakeGenericType(type);
+      return (ITranslateEntities<object>)Activator.CreateInstance(wrapperType, translator);
+    }
+
+    public class TranslateEntitiesWrapper<T> : ITranslateEntities<object>
+    {
+      readonly ITranslateEntities<T> _target;
+
+      public TranslateEntitiesWrapper(ITranslateEntities<T> target)
+      {
+        _target = target;
+      }
+
+      public MongoEntity<object> Translate(Document from)
+      {
+        var translated = _target.Translate(from);
+        return new MongoEntity<object>(translated.Entity, translated.Attributes);
+      }
+
+      public Document Translate(MongoEntity<object> from)
+      {
+        return _target.Translate(new MongoEntity<T>((T)from.Entity, from.Attributes));
+      }
     }
   }
 }
